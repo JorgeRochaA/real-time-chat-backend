215322bbd15f667e9037f109ef9daab764e413f0
Jorge Rocha <jor2019.cr@gmail.com>
2021-09-19 20:24:29 -0600

    register on UserController Ready

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,32 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
-    public function store(Request $request){
-         $user = $request->all();
+    public function store(Request $request)
+    {
+        $user = $request->all();
 
-         if ($request->username && $request->email && $request->password && $request->username_color && $request->user_picture ){
-              if (strlen($request->username) < 20){
-                $validateUsername = User::find($request->username);
-                if (!$validateUsername){
-                    $validateEmail = User::find($request->email);
-                    if (!$validateUsername){
+        if ($request->username && $request->email && $request->password && $request->username_color && $request->user_picture) {
+            if (strlen($request->username) < 20) {
+                $validateUsername = User::where("username", "=", $request->username)->first();
+                if (!$validateUsername) {
+                    $validateEmail = User::whereEmail($request->email)->first();
+                    if (!$validateEmail) {
                         $user['password'] = Hash::make($request->password);
                         User::create($user);
-                    }else{
-                        return response()->json(["error"=>"email already registered"]);
+                        $userRegistered = User::where("username", "=", $request->username)->first();
+                        $userRegistered->api_token = Str::random(100);
+                        $userRegistered->save();
+                        return response()->json(["token" => $userRegistered->api_token,
+                            "user" => ['username' => $userRegistered->username,
+                                'username_color' => $userRegistered->username_color,
+                                'user_picture' => $userRegistered->user_picture]]);
+                    } else {
+                        return response()->json(["error" => "email already registered"]);
                     }
-                }else{
-                    return response()->json(["error"=>"username already registered"]);
+                } else {
+                    return response()->json(["error" => "username already registered"]);
                 }
-              }else{
-                  return response()->json(["error"=>"username max length is 20 words"]);
-              }
-         }else{
-             return response()->json(["error"=>"Fill All The Fields"]);
-         }
+
+            } else {
+                return response()->json(["error" => "username max length is 20 words"]);
+            }
+        } else {
+            return response()->json(["error" => "Fill All The Fields"]);
+        }
     }
 }
