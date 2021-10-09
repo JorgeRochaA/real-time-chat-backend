26251799ae2175a7120bd984487d4151a4fe1cfc
JorgeRochaA <jor2018.cr@gmail.com>
2021-10-09 17:44:28 -0600

    send Message Action added

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function insertMessage(Request $request)
+    {
+        if ($request->message && $request->date && $request->hour && $request->username_id && $request->token) {
+            $user = User::find($request->username_id); // validate if the user exist
+            if ($user) {
+                if ($user->api_token == $request->token) { // validate token
+                    if (strlen($request->message) <= 148) {
+                        if (strlen($request->date) <= 10) {
+                            if (strlen($request->hour) <= 7) {
+                                $message = ["username" => $user->username,
+                                    "message" => $request->message, "date" => $request->date, "hour" => $request->hour,
+                                    "username_color" => $user->username_color];
+                                Message::create($message);
+                                return response()->json(["success" => "message created"]);
+                            } else {
+                                return response()->json(["error" => "Hour Max Length Is 7 Letters"]);
+                            }
+                        } else {
+                            return response()->json(["error" => "Date Max Length Is 10 Letters"]);
+                        }
+                    } else {
+                        return response()->json(["error" => "Message Max Length Is 148 Letters"]);
+                    }
+                } else {
+                    return response()->json(["error" => "invalid token"]);
+                }
+            } else {
+                return response()->json(["error" => "Invalid ID"]);
+            }
+        } else {
+            return response()->json(["error" => "Fill All The Filds"]);
+        }
+    }
+
+    public function getMessages(){
+        return response()->json(Message::all(),200);
+    }
+}
